4683ff37cca7c42701e8e9ccdcf94d572955f393
quantraynk-hub <quantraynk@gmail.com>
2026-03-09 04:27:21 +0530

    Update options_engine.py

--- engines/options_engine.py
+++ engines/options_engine.py
@@ -1,5 +1,14 @@
 def calculate_pcr(option_data):
 
+    if option_data is None:
+        return None
+
+    if "records" not in option_data:
+        return None
+
+    if "data" not in option_data["records"]:
+        return None
+
     total_put = 0
     total_call = 0
 
@@ -10,6 +19,9 @@ def calculate_pcr(option_data):
             total_call += item["CE"]["openInterest"]
             total_put += item["PE"]["openInterest"]
 
+    if total_call == 0:
+        return None
+
     pcr = total_put / total_call
 
-    return pcr
+    return round(pcr, 3)
