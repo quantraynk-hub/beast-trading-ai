b791fc9e3bf8bf85abc5eb25857a8b3842382828
quantraynk-hub <quantraynk@gmail.com>
2026-03-09 10:20:22 +0530

    Update nse_data.py

--- data_pipeline/nse_data.py
+++ data_pipeline/nse_data.py
@@ -1,42 +1,18 @@
-import requests
-
-session = requests.Session()
-
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br"
-}
-
-session.headers.update(headers)
-
+import yfinance as yf
 
 def get_nifty_price():
 
     try:
-        url = "https://www.nseindia.com/api/allIndices"
-        r = session.get(url)
-        data = r.json()
+        ticker = yf.Ticker("^NSEI")
+        data = ticker.history(period="1d")
 
-        for item in data["data"]:
-            if item["index"] == "NIFTY 50":
-                return item["last"]
+        price = data["Close"].iloc[-1]
+
+        return round(price,2)
 
     except:
         return None
 
 
 def get_option_chain():
-
-    try:
-        # first request generates cookies
-        session.get("https://www.nseindia.com")
-
-        url = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
-
-        r = session.get(url)
-
-        return r.json()
-
-    except:
-        return None
+    return None
